8139fad59449541b57ffa273b904ee5c226fee98
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2026-06-29 16:49:54 +0800

    fix URL issue

--- apps/backend/src/bin/seed.rs
+++ apps/backend/src/bin/seed.rs
@@ -15,7 +15,7 @@ async fn main() {
     println!("Seed is running!");
 
     let cluster_endpoint =
-        env::var("QDRANT_CLUSTER_ENDPOINT").unwrap_or_else(|_| "http://127.0.0.1:6333".to_string());
+        env::var("QDRANT_CLUSTER_ENDPOINT").unwrap_or_else(|_| "http://127.0.0.1:6334".to_string());
     let api_key = env::var("QDRANT_API_KEY").unwrap_or_else(|_| "not_needed".to_string());
 
     let client = Qdrant::from_url(cluster_endpoint.as_str())
