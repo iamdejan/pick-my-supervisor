14addeacb8bfd5e4009652ef22b875a77a2586a8
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2026-06-29 16:32:19 +0800

    temp

--- apps/backend/src/bin/seed.rs
+++ apps/backend/src/bin/seed.rs
@@ -1,3 +1,27 @@
+use std::env;
+
+use dotenvy;
+use qdrant_client::Qdrant;
+
 fn main() {
-    println!("Seed!")
+    dotenvy::dotenv().unwrap();
+
+    println!("Seed is running!");
+
+    let cluster_endpoint =
+        env::var("QDRANT_CLUSTER_ENDPOINT").unwrap_or_else(|_| "http://127.0.0.1:6333".to_string());
+
+    let api_key = env::var("QDRANT_API_KEY").unwrap_or_else(|_| "not_needed".to_string());
+    let client = Qdrant::from_url(cluster_endpoint.as_str())
+        .api_key(api_key)
+        .build()
+        .unwrap();
+    let does_collection_exist = async || {
+        client.collection_exists("{collection_name}").await
+    };
+    if does_collection_exist {
+        
+    }
+
+    println!("Seed is done!")
 }
