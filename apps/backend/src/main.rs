b609ab9dee66cb9245f3341e99d6cf14fcc073f7
Giovanni Dejan <15686093+iamdejan@users.noreply.github.com>
2026-06-30 00:08:49 +0800

    feat(backend): pick supervisor based on description (#2)

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -1,17 +1,35 @@
-use std::env;
+use std::{collections::HashMap, env};
 
-use axum::{Json, Router, routing::get};
+use axum::{
+    Json, Router,
+    routing::{get, post},
+};
+use qdrant_client::{
+    Qdrant,
+    qdrant::{Document, PointId, Query, QueryPointsBuilder, point_id::PointIdOptions},
+};
+use serde::{Deserialize, Serialize};
 use serde_json::{Value, json};
 use tower_http::cors::CorsLayer;
 
+#[derive(Serialize, Deserialize)]
+struct PickSupervisorRequest {
+    pub interesting_topics: Vec<String>,
+    pub additional_text: String,
+}
+
+static COLLECTION_NAME: &'static str = "lecturers";
+
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().unwrap();
+
     let app = Router::new()
-        .route("/api/health", get(health_check))
+        .route("/healthcheck", get(health_check))
+        .route("/supervisors/pick", post(pick_supervisor))
         .layer(CorsLayer::permissive()); // Allow all origins for dev
 
     let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
-    // Default to 127.0.0.1 for local safety, swap to 0.0.0.0 via env var in production
     let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
 
     let address = format!("{host}:{port}");
@@ -22,4 +40,80 @@ async fn main() {
 
 async fn health_check() -> Json<Value> {
     Json(json!({"status": "ok", "message": "Axum backend is running!"}))
+}
+
+fn extract_u64_from_point_id(point_id: &PointId) -> Option<u64> {
+    match &point_id.point_id_options {
+        // Matches if the PointId is a u64 number
+        Some(PointIdOptions::Num(num)) => Some(*num),
+
+        // Return None if the PointId is a UUID string
+        Some(PointIdOptions::Uuid(_uuid)) => None,
+
+        // Return None if the PointId is empty
+        None => None,
+    }
+}
+
+static TEMPLATE: &'static str = r#"
+Area of interest:
+{}
+
+Additional text:
+{}
+
+Based on the given information, who is the most suitable supervisor?
+"#;
+
+async fn pick_supervisor(Json(payload): Json<PickSupervisorRequest>) -> Json<Value> {
+    let interesting_topics_str: Vec<String> = payload
+        .interesting_topics
+        .iter()
+        .map(|item| format!("- {}", item))
+        .collect();
+    let interesting_topics_str = interesting_topics_str.join("\n");
+    let interesting_topics_str = interesting_topics_str.as_str();
+    let text = TEMPLATE.replacen("{}", interesting_topics_str, 1).replacen(
+        "{}",
+        payload.additional_text.as_str(),
+        1,
+    );
+
+    let cluster_endpoint =
+        env::var("QDRANT_CLUSTER_ENDPOINT").unwrap_or_else(|_| "http://127.0.0.1:6334".to_string());
+    let qdrant_api_key = env::var("QDRANT_API_KEY").unwrap_or_else(|_| "not_needed".to_string());
+    let openrouter_api_key =
+        env::var("OPENROUTER_API_KEY").unwrap_or_else(|_| "not_needed".to_string());
+    let qdrant_client = Qdrant::from_url(cluster_endpoint.as_str())
+        .api_key(qdrant_api_key)
+        .build()
+        .unwrap();
+    let query_response = qdrant_client
+        .with_header("openrouter-api-key", openrouter_api_key)
+        .query(
+            QueryPointsBuilder::new(COLLECTION_NAME)
+                .query(Query::new_nearest(Document {
+                    text: text,
+                    model: "openrouter/qwen/qwen3-embedding-8b".into(),
+                    options: HashMap::new(),
+                }))
+                .limit(2)
+                .with_payload(true)
+                .build(),
+        )
+        .await
+        .unwrap();
+    let results: Vec<Value> = query_response
+        .result
+        .iter()
+        .map(|item| {
+            let item_id = item.to_owned().id.unwrap();
+            return json!({
+                "id": extract_u64_from_point_id(&item_id).unwrap(),
+                "name": &item.payload.get("name").unwrap(),
+                "slug": &item.payload.get("slug").unwrap(),
+            });
+        })
+        .collect();
+    return Json(json!({"potential_supervisors": results}));
 }
